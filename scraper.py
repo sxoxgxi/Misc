69b6fcbde4a9b0a38fb8d2e7d0b1b7a6a6a6f6a7
sxoxgxi <nirmalrawal198@gmail.com>
2022-06-06 12:55:51 +0545

    f

--- scraper.py
+++ scraper.py
@@ -1,6 +1,21 @@
+from cgi import print_directory
+from wsgiref import headers
 import requests
+from bs4 import BeautifulSoup
 
 url = 'http://animixplay.to'
-response = requests.get(url)
-print(response.text)
-# classnaem= "iframecontainer"
+classname= "iframecontainer"
+
+
+headers = {
+    'user-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1',
+    'Accept-language': 'en-US',
+    'Referer': 'http://google.com',
+    'DNT': '1'
+}
+r = requests.get(url, headers=headers)
+# print(r.text)
+data = r.text
+soup = BeautifulSoup(data, 'html.parser')
+html = soup.prettify()
+# print(soup.find(class_=f'{classname}'))
